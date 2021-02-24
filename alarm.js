f54df6f407a4bc0f4f3e562b6f0a32c0c2f863d2
JongtaekChoi <lastchoice86@naver.com>
2021-02-24 14:20:59 +0900

    implements simple

--- alarm.js
+++ alarm.js
@@ -1,2 +1,10 @@
+const { exec } = require("child_process");
+
 const args = process.argv.slice(2);
-console.log(args);
+const [ timeout, phrase ] = args;
+setTimeout(() => {
+  const command = `say ${phrase || "hello!"}`;
+  console.log(command);
+  exec(command);
+  process.exit();
+}, timeout * 1000);
